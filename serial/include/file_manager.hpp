f4a1f59072b26011fb419172c63ee4c12e288903
Clanz99 <lanzaro.claudio@gmail.com>
2024-08-06 16:53:39 +0200

    Mega update

---
+++ serial/include/file_manager.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+int readGraphFromFile(string, int*, vector<vector<int>>*);
---
+++ serial/main.cpp
@@ -0,0 +1,12 @@
+#include "include/file_manager.hpp"
+#include "include/push_relabel_serial_basic.cuh"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string filename = "input_data/input.txt";
+    int maxflow = executePushRelabel(filename);
+    cout<<"Max flow: " <<maxflow <<endl;
+    return 0;
+}
