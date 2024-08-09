8eab30bd6954415d558af75783548bd902dab548
Clanz99 <lanzaro.claudio@gmail.com>
2024-08-09 22:21:42 +0200

    Generazione grafi

--- serial/testserial.sh
+++ serial/testserial.sh
@@ -4,4 +4,4 @@
 
 ./serial/prserial "input_data/graph3.txt" "results/graph3_serial_results.json"
 
-./serial/prserial "input_data/graph4.txt" "results/graph4_serial_results.json"
+#./serial/prserial "input_data/graph4.txt" "results/graph4_serial_results.json"
