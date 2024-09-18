09ad4297dc325a1453afdefb47b96d8b073dece5
Clanz99 <lanzaro.claudio@gmail.com>
2024-09-18 15:04:16 +0200

    Riorganizzazione

--- parallel/src/push_relabel_parallel.cu
+++ parallel/src/push_relabel_parallel.cu
@@ -1,130 +1,152 @@
 #include "../include/push_relabel_parallel.cuh"
 
-static void HandleError(cudaError_t err, const char *file, int line) {
-    if (err != cudaSuccess) {
-        printf("%s in %s at line %d\n", cudaGetErrorString(err), file, line);
-        exit(EXIT_FAILURE);
-    }
-}
- 
-#define HANDLE_ERROR(err) (HandleError(err, __FILE__, __LINE__))
+using namespace cooperative_groups;
 
-void checkCUDAError(const char *msg) {
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        fprintf(stderr, "CUDA error: %s: %s.\n", msg, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-}
+void preflow(int V, int source, int sink, int *capacities, int *residual, int *height, int *excess, int *totalExcess){
 
-void initialize(int *capacity, int *excess, int *height, int *residual, int *totalExcess, int n, int s){
-    for (int i = 0; i < n; i++){
-        height[i] = 0;
+    // Inizializzazione di height e excess flow
+    for(int i = 0; i < V; i++)
+    {
+        height[i] = 0; 
         excess[i] = 0;
     }
     
-    height[s] = n;
+    height[source] = V;
     *totalExcess = 0;
 
-    // Inizializzazione residui
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            residual[i*n + j] = capacity[i*n + j];
-        }
+    // Inizializzazione del preflow da source a tutti i nodi adiacenti
+    for(int i = 0; i < V; i++)
+    {
+        if(capacities[source*V + i] > 0)
+        {
+            residual[source*V + i] = 0;
+            residual[i*V + source] = capacities[source*V + i] + capacities[i*V + source];
+            
+            excess[i] = capacities[source*V + i];
+            *totalExcess += excess[i];
+        } 
     }
 
-    // Calcolo del preflusso
-    for (int i = 0; i < n; i++){
-        if (capacity[s*n + i] > 0){
-            residual[s*n + i] = residual[s*n + i] - capacity[s*n + i];
-            residual[i*n + s] = residual[i*n + s] + capacity[s*n + i];
-            excess[i] = capacity[s*n + i];
-            *totalExcess = *totalExcess + capacity[s*n + i];
-        }
-    }
 }
 
-__global__ void pushKernel(int *d_capacity, int *d_excess, int *d_height, int *d_residual, int n){
-    int u = blockIdx.x * blockDim.x + threadIdx.x;
-    if(u < n && u != 0 && u != n-1){
-        int cycle = n; //KERNEL_CYCLES
-        int e1, h1, h2, v1, delta;
+__global__ void pushRelabelKernel(int V, int source, int sink, int *d_capacities, int *d_residual, int *d_height,int *d_excess){
+    grid_group grid = this_grid();
+    unsigned int idx = (blockIdx.x*blockDim.x) + threadIdx.x;
 
-        while (cycle > 0){
-            if(d_excess[u] > 0 && d_height[u] < n){
+    int cycle = V;
+
+    while (cycle > 0) {
+
+        for (int u = idx; u < V; u += blockDim.x * gridDim.x) {
+
+            int e1, h1, h2, delta;
+            int v,v1;
+
+            if( (d_excess[u] > 0) && (d_height[u] < V) && u != source && u != sink){
                 e1 = d_excess[u];
                 h1 = INF;
-                v1 = NULL;
-                for (int v = 0; v < n; v++){
-                    if(d_residual[u*n + v] > 0){
+                v1 = -1;
+
+                for(v = 0; v < V; v++){
+
+                    if(d_residual[u*V + v] > 0){
                         h2 = d_height[v];
                         if(h2 < h1){
                             v1 = v;
                             h1 = h2;
                         }
-                    }                
+                    }
                 }
-                if(d_height[u] > h1){
-                    delta = min(e1, d_residual[u*n + v1]);
-                    atomicAdd(&d_residual[v1*n + u], delta);
-                    atomicSub(&d_residual[u*n + v1], delta);
-                    atomicAdd(&d_excess[v1], delta);
-                    atomicSub(&d_excess[u], delta);
-                }
-                else{
-                    d_height[u] = h1 + 1;
+                if (v1 == -1) {
+                    d_height[u] = V;
+                } else {
+                    if(d_height[u] > h1){
+                        
+                        //TODO: usare min
+                        delta = e1;
+                        if(e1 > d_residual[u*V + v1]){
+                            delta = d_residual[u*V + v1];
+                        }
+
+                        atomicAdd(&d_residual[v1*V + u],delta);
+                        atomicSub(&d_residual[u*V + v1],delta);
+
+                        atomicAdd(&d_excess[v1],delta);
+                        atomicSub(&d_excess[u],delta);
+                    } else {
+                        d_height[u] = h1 + 1;
+                    }
                 }
             }
-            cycle = cycle - 1;
         }
+
+        cycle = cycle - 1;
+        grid.sync();
     }
 }
 
-void globalRelabel(int *capacity, int *excess, int *height, int *residual, int *totalExcess, bool *scanned, bool *mark, int n, int t){
-    for (int u = 0; u < n; u++){
-        for (int v = 0; v < n; v++){
-            if(capacity[u*n + v] > 0){
-                if(height[u] > height[v] + 1){
-                    excess[u] = excess[u] - residual[u*n + v];
-                    excess[v] = excess[v] + residual[u*n + v];
-                    residual[v*n + u] = residual[v*n + u] + residual[u*n + v];
-                    residual[u*n + v] = 0;
+void globalRelabel(int V, int source, int sink, int *capacities, int *residual, int *height, int *excess, int *totalExcess, bool *mark, bool *scanned){
+    for(int u = 0; u < V; u++){
+        for(int v = 0; v < V; v++){
+            if(capacities[u*V + v] > 0)
+            {
+                if(height[u] > height[v] + 1)
+                {
+                    excess[u] = excess[u] - residual[u*V + v];
+                    excess[v] = excess[v] + residual[u*V + v];
+                    residual[v*V + u] = residual[v*V + u] + residual[u*V + v];
+                    residual[u*V + v] = 0;
                 }
             }
         }
     }
-    
-    std::list<int> queue;
-    int x, y, current;
-    
-    for (int i = 0; i < n; i++){
+
+    // BFS
+    std::list<int> Queue;
+    int x,y,current;
+        
+    for(int i = 0; i < V; i++)
+    {
         scanned[i] = false;
     }
-    queue.push_back(t);
-    scanned[t] = true;
-    height[t] = 0;
-    while (!queue.empty()){
-        x = queue.front();
-        queue.pop_front();
-        current = height[x] +1;
-        for(y = 0; y < n; y++){
-            if(residual[y*n + x] > 0 && !scanned[y]){
-                height[y] = current;
-                scanned[y] = true;
-                queue.push_back(y);
+
+    // Accodo sink
+    Queue.push_back(sink);
+    scanned[sink] = true;
+    height[sink] = 0;
+
+    while(!Queue.empty())
+    {
+        x = Queue.front();
+        Queue.pop_front();
+
+        current = height[x];
+        current = current + 1;
+
+        for(y = 0; y < V; y++){
+            if(residual[y*V+x] > 0){
+                if(scanned[y] == false){
+                    height[y] = current;
+                    scanned[y] = true;
+                    Queue.push_back(y);
+                }
             }
         }
     }
-    
+
+    // Controllo se tutti i nodi sono stati scansionati
     bool allScanned = true;
-    for(int i = 0; i < n; i++){
-        if(scanned[i]==false){
+    for(int i = 0; i < V; i++){
+        if(scanned[i] == false){
             allScanned = false;
             break;
         }
     }
-    if(allScanned==false){
-        for (int i = 0; i < n; i++){
+
+    // Se non tutti i nodi sono stati scansionati...
+    if(allScanned == false){
+        for(int i = 0; i < V; i++){
+
             if(!(scanned[i] || mark[i])){
                 mark[i] = true;
                 *totalExcess = *totalExcess - excess[i];
@@ -133,126 +155,129 @@ void globalRelabel(int *capacity, int *excess, int *height, int *residual, int *
     }
 }
 
-int pushRelabel(int *capacity, int *excess, int *height, int *residual, int *d_capacity, int *d_excess, int *d_height, int *d_residual, int *totalExcess, int n, int s, int t){
-    bool *scanned = (bool*)malloc(n*sizeof(bool));
-    bool *mark = (bool*)malloc(n*sizeof(bool));
-    for (int i = 0; i < n; i++){
+void pushRelabel(int V, int source, int sink, int *capacities, int *residual, int *height, int *excess, int *totalExcess, int *d_capacities, int *d_residual, int *d_height, int *d_excess){
+    
+    // Dichiarazione delle variabili per global relabel
+    bool *mark,*scanned;
+    mark = (bool*)malloc(V*sizeof(bool));
+    scanned = (bool*)malloc(V*sizeof(bool));
+
+    // Configure the GPU
+    int device = -1;
+    cudaGetDevice(&device);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, device);
+    dim3 num_blocks(deviceProp.multiProcessorCount); // un blocco per ogni SM
+    dim3 block_size(256); // 256 threads per blocco
+
+    void* kernel_args[] = {&V,&source,&sink,&d_capacities,&d_residual,&d_height,&d_excess};
+
+    // initialising mark values to false for all nodes
+    for(int i = 0; i < V; i++)
+    {
         mark[i] = false;
     }
 
-    dim3 blockSize(256);
-    dim3 gridSize((n + blockSize.x - 1) / blockSize.x);
-    
-    while ((excess[s]+excess[t])<*totalExcess){
+    while((excess[source] + excess[sink]) < *totalExcess)
+    {
+        // Trasferimento dati da host a device
+        HANDLE_ERROR(cudaMemcpy(d_height,height,V*sizeof(int),cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(d_excess, excess,V*sizeof(int), cudaMemcpyHostToDevice));
+        HANDLE_ERROR(cudaMemcpy(d_residual,residual,V*V*sizeof(int),cudaMemcpyHostToDevice));
 
-        std::cout << "Source excess: " << excess[s] << " Sink excess: " << excess[t] << " Total excess: " << *totalExcess << std::endl;
+        cudaError_t cudaStatus;
+        cudaStatus = cudaLaunchCooperativeKernel((void*)pushRelabelKernel, num_blocks, block_size, kernel_args, 0, 0);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "cudaLaunchCooperativeKernel failed: %s\n", cudaGetErrorString(cudaStatus));
+            // Handle the error, for example, by cleaning up resources and exiting
+            exit(1);
+        }
 
-        if(_DEBUG) std::cout << "Push..." << std::endl;
-        //HANDLE_ERROR(cudaMemcpy(d_excess, excess, n*sizeof(int), cudaMemcpyHostToDevice));
-        HANDLE_ERROR(cudaMemcpy(d_height, height, n*sizeof(int), cudaMemcpyHostToDevice));
-        //HANDLE_ERROR(cudaMemcpy(d_residual, residual, n*n*sizeof(int), cudaMemcpyHostToDevice));
-        pushKernel<<<gridSize, blockSize>>>(d_capacity, d_excess, d_height, d_residual, n);
         cudaDeviceSynchronize();
-        if(_DEBUG) std::cout << "Push done" << std::endl;
 
-        HANDLE_ERROR(cudaMemcpy(excess, d_excess, n*sizeof(int), cudaMemcpyDeviceToHost));
-        HANDLE_ERROR(cudaMemcpy(height, d_height, n*sizeof(int), cudaMemcpyDeviceToHost));
-        HANDLE_ERROR(cudaMemcpy(residual, d_residual, n*n*sizeof(int), cudaMemcpyDeviceToHost));
+        cudaError_t err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            fprintf(stderr, "CUDA error: %s.\n", cudaGetErrorString(err));
+            exit(EXIT_FAILURE);
+        }
 
-        if(_DEBUG) printStatus(capacity, excess, height, residual, totalExcess, n);
-
-        if(_DEBUG) std::cout << "Global relabel..." << std::endl;
-        globalRelabel(capacity, excess, height, residual, totalExcess, scanned, mark, n, t);
-        if(_DEBUG) std::cout << "Global relabel done" << std::endl;
+        // Trasferimento dati da device a host
+        HANDLE_ERROR(cudaMemcpy(height,d_height,V*sizeof(int),cudaMemcpyDeviceToHost));
+        HANDLE_ERROR(cudaMemcpy(excess,d_excess,V*sizeof(int),cudaMemcpyDeviceToHost));
+        HANDLE_ERROR(cudaMemcpy(residual,d_residual,V*V*sizeof(int),cudaMemcpyDeviceToHost));
         
-        if(_DEBUG) printStatus(capacity, excess, height, residual, totalExcess, n);
-        
+        // Global relabel
+        globalRelabel(V,source,sink,capacities,residual,height,excess,totalExcess,mark,scanned);
     }
-    return excess[t];
 }
 
-std::vector<int> findMinCutSetFromT(int n, int t, int *residual){
-    std::vector<int> minCutSet;
-    std::queue<int> q;
-    std::vector<bool> visited(n, false);
-    minCutSet.push_back(t);
-    q.push(t);
-    visited[t] = true;
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (int v = 0; v < n; ++v) {
-            if (!visited[v] && residual[v*n + u] > 0) {
-                minCutSet.push_back(v);
-                q.push(v);
-                visited[v] = true;
+int executePushRelabel(std::string filename, std::string output){
+    
+    // Dichiarazione delle variabili host
+    int V, E, source, sink;
+    int *capacities, *residual, *height, *excess;
+    int *totalExcess;
+
+    // Dichiarazione delle variabili device
+    int *d_capacities, *d_residual, *d_height, *d_excess;
+
+    // Lettura del grafo da file
+    readGraphFromFile(filename, V, &capacities);
+    
+    // Inizializzazione delle variabili source e sink
+    source = 0;
+    sink = V - 1;
+
+    // Inizializzazione delle variabili E e residual
+    residual = (int*)malloc(V*V*sizeof(int));
+
+    for(int i = 0; i < V; i++){
+        for(int j = 0; j < V; j++){
+            residual[i*V + j] = capacities[i*V + j];
+            if(capacities[i*V + j] > 0){
+                E = E + 1;
             }
         }
     }
 
-    return minCutSet;
-}
+    // Inizializzazione delle variabili height e excess
+    height = (int*)malloc(V*sizeof(int));
+    excess = (int*)malloc(V*sizeof(int));
+    totalExcess = (int*)malloc(sizeof(int));
 
-int executePushRelabel(std::string filename, std::string output){
-    int n = 0;
-    int *capacity = nullptr;
+    // Allocazione delle variabili device
+    HANDLE_ERROR(cudaMalloc((void**)&d_capacities,V*V*sizeof(int)));
+    HANDLE_ERROR(cudaMalloc((void**)&d_residual,V*V*sizeof(int)));
+    HANDLE_ERROR(cudaMalloc((void**)&d_height,V*sizeof(int)));
+    HANDLE_ERROR(cudaMalloc((void**)&d_excess,V*sizeof(int)));
 
-    // Lettura grafo da file
-    readGraphFromFile(filename, n, &capacity);
+    // Calcolo preflow
+    preflow(V,source,sink,capacities,residual,height,excess,totalExcess);
 
-    const auto start = std::chrono::high_resolution_clock::now();
+    // Trasferimento dati da host a device
+    HANDLE_ERROR(cudaMemcpy(d_height,height,V*sizeof(int),cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(d_excess,excess,V*sizeof(int),cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(d_capacities,capacities,V*V*sizeof(int),cudaMemcpyHostToDevice));
+    HANDLE_ERROR(cudaMemcpy(d_residual,residual,V*V*sizeof(int),cudaMemcpyHostToDevice));
 
-    int s = 0;
-    int t = n-1;
-    int *flow = (int *)malloc(n*n*sizeof(int));
-    int *excess = (int *)malloc(n*sizeof(int));
-    int *height = (int *)malloc(n*sizeof(int));
-    int *residual = (int *)malloc(n*n*sizeof(int));
-    int *totalExcess = (int *)malloc(sizeof(int));
-
-    int *d_capacity, *d_excess, *d_height, *d_residual;    
-
-    HANDLE_ERROR(cudaMalloc((void**)&d_capacity, n*n*sizeof(int)));
-    HANDLE_ERROR(cudaMalloc((void**)&d_excess, n*sizeof(int)));
-    HANDLE_ERROR(cudaMalloc((void**)&d_height, n*sizeof(int)));
-    HANDLE_ERROR(cudaMalloc((void**)&d_residual, n*n*sizeof(int)));
-
-    initialize(capacity, excess, height, residual, totalExcess, n, s);
-
-    if(_DEBUG) std::cout << "Initialization done" << std::endl;
-    if(_DEBUG) printStatus(capacity, excess, height, residual, totalExcess, n);
+    pushRelabel(V,source,sink,capacities,residual,height,excess,totalExcess,d_capacities,d_residual,d_height,d_excess);
     
-    const auto endInitialization = std::chrono::high_resolution_clock::now();
 
-    HANDLE_ERROR(cudaMemcpy(d_capacity, capacity, n*n*sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(d_excess, excess, n*sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(d_height, height, n*sizeof(int), cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(d_residual, residual, n*n*sizeof(int), cudaMemcpyHostToDevice));
+    // Stampa del flusso massimo
+    printf("The maximum flow value of this flow network as calculated by the parallel implementation is %d\n",excess[sink]);
     
-    if(_DEBUG) std::cout << "Push relabel..." << std::endl;
-    int maxFlow = pushRelabel(capacity, excess, height, residual, d_capacity, d_excess, d_height, d_residual, totalExcess, n, s, t);
-    const auto end = std::chrono::high_resolution_clock::now();
-    if(_DEBUG) std::cout << "Push relabel done" << std::endl;
-    
-    //std::cout<<"Max flow: "<<maxFlow<<std::endl;
 
-    auto initializationTime = std::chrono::duration_cast<std::chrono::microseconds>(endInitialization - start);
-    auto executionTime = std::chrono::duration_cast<std::chrono::microseconds>(end - endInitialization);
-    auto totalTime = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-    std::vector<int> minCut = findMinCutSetFromT(n, t, residual);
-    writeResultsToFile(output, excess[t], minCut, initializationTime, executionTime, totalTime);
-
-    cudaFree(d_capacity);
-    cudaFree(d_excess);
-    cudaFree(d_height);
+    // Liberazione della memoria
+    cudaFree(d_capacities);
     cudaFree(d_residual);
-
-    free(capacity);
-    free(flow);
-    free(excess);
-    free(height);
+    cudaFree(d_height);
+    cudaFree(d_excess);
+    
+    free(capacities);
     free(residual);
+    free(height);
+    free(excess);
+    free(totalExcess);
 
     return 0;
 }
