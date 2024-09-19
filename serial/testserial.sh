7dffe48bc4eb6df44be3a756a1a94c63343a46c3
Daniele974 <milodani99@gmail.com>
2024-09-19 15:56:07 +0200

    Riordinati test in testserial.sh

--- serial/testserial.sh
+++ serial/testserial.sh
@@ -4,20 +4,20 @@
 
 ./serial/prserial "input_data/graph3.txt" "results/graph3_serial_results.json"
 
-./serial/prserial "input_data/graph4.txt" "results/graph4_serial_results.json"
-
-./serial/prserial "input_data/graph5.txt" "results/graph5_serial_results.json"
-
-./serial/prserial "input_data/graph6.txt" "results/graph6_serial_results.json"
-
-./serial/prserial "input_data/graph7.txt" "results/graph7_serial_results.json"
-
 ./serial/prserial "input_data/graph3b.txt" "results/graph3b_serial_results.json"
+
+./serial/prserial "input_data/graph4.txt" "results/graph4_serial_results.json"
 
 ./serial/prserial "input_data/graph4b.txt" "results/graph4b_serial_results.json"
 
+./serial/prserial "input_data/graph5.txt" "results/graph5_serial_results.json"
+
 ./serial/prserial "input_data/graph5b.txt" "results/graph5b_serial_results.json"
 
+./serial/prserial "input_data/graph6.txt" "results/graph6_serial_results.json"
+
 ./serial/prserial "input_data/graph6b.txt" "results/graph6b_serial_results.json"
+
+./serial/prserial "input_data/graph7.txt" "results/graph7_serial_results.json"
 
 ./serial/prserial "input_data/graph7b.txt" "results/graph7b_serial_results.json"
