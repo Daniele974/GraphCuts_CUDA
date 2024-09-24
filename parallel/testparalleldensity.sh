4acc45e764d5e5577e3f1041276c67ac86d7f655
Daniele974 <milodani99@gmail.com>
2024-09-24 15:36:34 +0200

    Aggiornati makefile

--- parallel/testparalleldensity.sh
+++ parallel/testparalleldensity.sh
@@ -1,20 +1,20 @@
-./parallel/prparallel "input_data/densitytest/graphP01.txt" "results/graphP01_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP01.txt" "results/graphP01_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP02.txt" "results/graphP02_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP02.txt" "results/graphP02_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP03.txt" "results/graphP03_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP03.txt" "results/graphP03_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP04.txt" "results/graphP04_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP04.txt" "results/graphP04_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP05.txt" "results/graphP05_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP05.txt" "results/graphP05_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP06.txt" "results/graphP06_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP06.txt" "results/graphP06_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP07.txt" "results/graphP07_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP07.txt" "results/graphP07_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP08.txt" "results/graphP08_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP08.txt" "results/graphP08_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP09.txt" "results/graphP09_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP09.txt" "results/graphP09_parallel_results.json" 0
 
-./parallel/prparallel "input_data/densitytest/graphP1.txt" "results/graphP1_parallel_results.json"
+./parallel/prparallel "input_data/densitytest/graphP1.txt" "results/graphP1_parallel_results.json" 0
 
--- parallel_bcsr_vc/testparallelbcsrdensity.sh
+++ parallel_bcsr_vc/testparallelbcsrdensity.sh
@@ -1,20 +1,20 @@
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP01.txt" "results/graphP01_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP01.txt" "results/graphP01_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP02.txt" "results/graphP02_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP02.txt" "results/graphP02_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP03.txt" "results/graphP03_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP03.txt" "results/graphP03_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP04.txt" "results/graphP04_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP04.txt" "results/graphP04_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP05.txt" "results/graphP05_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP05.txt" "results/graphP05_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP06.txt" "results/graphP06_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP06.txt" "results/graphP06_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP07.txt" "results/graphP07_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP07.txt" "results/graphP07_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP08.txt" "results/graphP08_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP08.txt" "results/graphP08_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP09.txt" "results/graphP09_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP09.txt" "results/graphP09_parallelbcsr_results.json" 0
 
-./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP1.txt" "results/graphP1_parallelbcsr_results.json"
+./parallel_bcsr_vc/prparallelbcsr "input_data/densitytest/graphP1.txt" "results/graphP1_parallelbcsr_results.json" 0
 
