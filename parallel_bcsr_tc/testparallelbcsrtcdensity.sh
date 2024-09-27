8859780b9e0d93ee765470065f7d307ff75d26bb
Daniele974 <milodani99@gmail.com>
2024-09-27 14:48:01 +0200

    Risultati test session 8 density 1K

--- parallel_bcsr_tc/testparallelbcsrtcdensity.sh
+++ parallel_bcsr_tc/testparallelbcsrtcdensity.sh
@@ -1,20 +1,20 @@
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP01.txt" "results/graphP01_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP01.txt" "results/graphP01_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP02.txt" "results/graphP02_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP02.txt" "results/graphP02_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP03.txt" "results/graphP03_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP03.txt" "results/graphP03_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP04.txt" "results/graphP04_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP04.txt" "results/graphP04_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP05.txt" "results/graphP05_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP05.txt" "results/graphP05_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP06.txt" "results/graphP06_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP06.txt" "results/graphP06_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP07.txt" "results/graphP07_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP07.txt" "results/graphP07_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP08.txt" "results/graphP08_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP08.txt" "results/graphP08_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP09.txt" "results/graphP09_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP09.txt" "results/graphP09_parallelbcsrtc_results.json" 0
 
-./parallel/prparallelbcsrtc "input_data/densitytest1K/graphP1.txt" "results/graphP1_parallelbcsrtc_results.json" 0
+./parallel_bcsr_tc/prparallelbcsrtc "input_data/densitytest1K/graphP1.txt" "results/graphP1_parallelbcsrtc_results.json" 0
 
